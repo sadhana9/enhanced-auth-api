38b5ba12e2e16214b75f01142085511be5e2cb5a
Sadhana <sadhanapoojary18@gmail.com>
2024-05-23 19:07:57 +0530

    Initial commit

--- db.js
+++ db.js
@@ -3,16 +3,35 @@ require('dotenv').config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
+    const uri = process.env.MONGO_URI || 'mongodb://localhost:27017/sadhana';
+    await mongoose.connect(uri, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
     console.log('MongoDB connected...');
   } catch (err) {
-    console.error(err.message);
+    console.error('MongoDB connection error:', err.message);
     process.exit(1);
   }
+
+  mongoose.connection.on('connected', () => {
+    console.log('Mongoose connected to MongoDB');
+  });
+
+  mongoose.connection.on('error', (err) => {
+    console.error('Mongoose connection error:', err.message);
+  });
+
+  mongoose.connection.on('disconnected', () => {
+    console.log('Mongoose disconnected');
+  });
+
+  process.on('SIGINT', () => {
+    mongoose.connection.close(() => {
+      console.log('Mongoose connection closed due to application termination');
+      process.exit(0);
+    });
+  });
 };
 
 module.exports = connectDB;
-
